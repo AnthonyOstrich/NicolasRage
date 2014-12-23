547508c5b14006689876639a372f0b00d27b0c55
AnthonyOstrich <anthonyostrich@gmail.com>
2014-12-23 15:43:54 -0500

    Default Project

---
+++ android/src/anthonyostrich/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package anthonyostrich.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import anthonyostrich.NicolasRage;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NicolasRage(), config);
+	}
+}
---
+++ desktop/src/anthonyostrich/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package anthonyostrich.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import anthonyostrich.NicolasRage;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NicolasRage(), config);
+	}
+}
